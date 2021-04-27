a25f25e59a5d752dcbda2db0a25193576aaff589
Walid Zender <zender.walid@gmail.com>
2021-04-28 00:19:47 +0200

    debut création plateau

---
+++ script.js
@@ -0,0 +1,10 @@
+let i = 0;
+let colonnes = ['A', 'B', 'C', 'D', 'E', 'F', 'G','H'];
+let lignes = [];
+function createGameBoard() {
+    for (i = 1; i <= 8; i++) {
+        console.log(i);
+    }
+};
+
+createGameBoard();
